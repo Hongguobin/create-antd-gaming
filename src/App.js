2b2559b90e8a1ade1c65d98ee4cc70799054fce1
Hongguobin <1304098549@qq.com>
2021-02-24 16:54:57 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd'
+import './App.less';
+import React, { useReducer } from 'react'
+import dayjs from 'dayjs'
 
 function App() {
+  const [count, dispatch] = useReducer((state, action) => {
+    switch (action) {
+      case 'add':
+        const time = dayjs(1614149318642).format('YYYY-MM')
+        console.log(time)
+        return state + 1
+      default:
+        return
+    }
+  }, 0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <Button type="primary" onClick={() => dispatch('add')}>click me</Button>
+        <p className="text">
+          {count} <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
