1df4a9d99aac1ce0626ce1afe51e5bf2dac745bb
Hongguobin <1304098549@qq.com>
2021-03-02 16:34:41 +0800

    增加useEffect

--- src/page/discover/recommend/index.js
+++ src/page/discover/recommend/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Recommend() {
+    useEffect(() => {
+        console.log('欢迎来到推荐页面')
+        return () => {
+            console.log('走出推荐页面')
+        }
+    }, [])
     return (
         <div>推荐</div>
     )
