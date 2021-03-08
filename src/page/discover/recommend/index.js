2263eac94048720de965774f5c7241cf03c3d6c4
Mr.hong <1304098549@qq.com>
2021-03-08 22:40:58 +0800

    待修改

--- src/page/discover/recommend/index.js
+++ src/page/discover/recommend/index.js
@@ -1,37 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import './index.less'
-// import Swiper from 'swiper'
-// import 'swiper/css/swiper.css'
+import Swiper from 'swiper'
+import 'swiper/swiper-bundle.css'
+import p1 from '../../../assets/images/p1.jpg'
+import p2 from '../../../assets/images/p2.jpg'
+import p3 from '../../../assets/images/p3.jpg'
 
 const mapStateToProps = () => {
-    return {
-    }
+  return {}
 }
 
 function Recommend() {
-    useEffect(() => {
-        instanceSwiper()
-    }, [])
-    const instanceSwiper = () => {
-        // this.swiperObj = new Swiper('.swiper-container', {
-        //     slidesPerView: 1,
-        //     loop: false,
-        //     autoplay: {// 自动滑动
-        //         delay: 3000, //3秒切换一次
-        //         // stopOnLastSlide: false,
-        //         disableOnInteraction: false,//
-        //     },
-        //     observer: true,//修改swiper自己或子元素时，自动初始化swiper    重要
-        //     observeParents: true,//修改swiper的父元素时，自动初始化swiper  重要
-        // })
-    }
+  useEffect(() => {
+    instanceSwiper()
+  }, [])
+  const instanceSwiper = () => {
+    new Swiper ('.swiper-container', {
+        loop: true, // 循环模式选项
+      })
+  }
 
-    return (
-        <div className="reacommend">
-            <div className="swiper-container"></div>
+  return (
+    <div className="reacommend">
+        <div className="swiper-container">
+            <div className="swiper-wrapper">
+                <div className="swiper-slide">
+                    <img src={p1} alt=""></img>
+                </div>
+                <div className="swiper-slide">
+                    <img src={p2} alt=""></img>
+                </div>
+                <div className="swiper-slide">
+                    <img src={p3} alt=""></img>
+                </div>
+            </div>
+            <div className="swiper-pagination"></div>
+
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div>
+
+            <div className="swiper-scrollbar"></div>
         </div>
-    )
+    </div>
+  )
 }
 export default connect(mapStateToProps)(withRouter(Recommend))
