ef5ab5c0e5d1bccbc443c8ca1d36b6b2bd501bb3
Hongguobin <1304098549@qq.com>
2021-03-08 18:46:18 +0800

    引入swiper

--- src/page/discover/recommend/index.js
+++ src/page/discover/recommend/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import './index.less'
+// import Swiper from 'swiper'
+// import 'swiper/css/swiper.css'
 
 const mapStateToProps = () => {
     return {
@@ -9,9 +12,26 @@ const mapStateToProps = () => {
 
 function Recommend() {
     useEffect(() => {
-    })
+        instanceSwiper()
+    }, [])
+    const instanceSwiper = () => {
+        // this.swiperObj = new Swiper('.swiper-container', {
+        //     slidesPerView: 1,
+        //     loop: false,
+        //     autoplay: {// 自动滑动
+        //         delay: 3000, //3秒切换一次
+        //         // stopOnLastSlide: false,
+        //         disableOnInteraction: false,//
+        //     },
+        //     observer: true,//修改swiper自己或子元素时，自动初始化swiper    重要
+        //     observeParents: true,//修改swiper的父元素时，自动初始化swiper  重要
+        // })
+    }
+
     return (
-        <div>推荐</div>
+        <div className="reacommend">
+            <div className="swiper-container"></div>
+        </div>
     )
 }
 export default connect(mapStateToProps)(withRouter(Recommend))
